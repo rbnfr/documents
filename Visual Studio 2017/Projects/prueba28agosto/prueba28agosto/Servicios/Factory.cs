009de7614d75a2e0db81f13f3f6b9a98ca15ef52
PC <PC@DESKTOP-613>
2017-08-28 14:17:59 +0200

    Actualizacion

---
+++ Visual Studio 2017/Projects/prueba28agosto/prueba28agosto/Servicios/Factory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prueba28agosto.Servicios
+{
+    class Factory
+    {
+        private object CreateFactory(object obj)
+        {
+            var type = obj.GetType();
+            if (null == type)
+            {
+                throw new Exception("El tipo del objeto no existe");
+            }
+
+            // Crear instancia
+            var instance = new type();
+        }
+    }
+}
