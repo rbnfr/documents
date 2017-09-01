1ab3e0433f4e195562c725bc1c6a5c4479e59afb
PC <PC@DESKTOP-613>
2017-09-01 13:35:00 +0200

    Prueba EntityFramework

---
+++ Visual Studio 2017/Projects/pruebaDBcontext/pruebaDBcontext/pizza.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebaDBcontext
+{
+    class pizza
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public List<ingredient> Ingredientes { get; set; }
+        public decimal Precio { get; set; }
+    }
+}
